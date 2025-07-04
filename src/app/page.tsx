feef4c0da8b15d76a3fc3f472308ec7625df4e68
Ly Dollar <146605742+DollieNOMONEY@users.noreply.github.com>
2025-07-04 18:14:11 +0700

    Fixed ESLint Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Navigation from "./components/Navigation";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const videos = [
